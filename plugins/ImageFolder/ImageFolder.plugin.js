422206f7b334a495a8da5216dc029b0985454e1c
TheLazySquid <thelazysquid2@gmail.com>
2025-12-10 22:42:05 -0500

    Fix ImageFolder button colors too

--- plugins/ImageFolder/ImageFolder.plugin.js
+++ plugins/ImageFolder/ImageFolder.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name ImageFolder
  * @description A BetterDiscord plugin that allows you to save and send images from a folder for easy access
- * @version 1.4.10
+ * @version 1.4.11
  * @author TheLazySquid
  * @authorId 619261917352951815
  * @website https://github.com/TheLazySquid/BetterDiscordPlugins
@@ -34,7 +34,7 @@ var __toBinary = /* @__PURE__ */ (() => {
 })();
 
 // assets/image-plus-outline.svg
-var image_plus_outline_default = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="var(--__lottieIconColor,var(--interactive-text-default))"><path d="M13 19C13 19.7 13.13 20.37 13.35 21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H19C20.11 3 21 3.9 21 5V13.35C20.37 13.13 19.7 13 19 13V5H5V19H13M13.96 12.29L11.21 15.83L9.25 13.47L6.5 17H13.35C13.75 15.88 14.47 14.91 15.4 14.21L13.96 12.29M20 18V15H18V18H15V20H18V23H20V20H23V18H20Z" /></svg>';
+var image_plus_outline_default = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13 19C13 19.7 13.13 20.37 13.35 21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H19C20.11 3 21 3.9 21 5V13.35C20.37 13.13 19.7 13 19 13V5H5V19H13M13.96 12.29L11.21 15.83L9.25 13.47L6.5 17H13.35C13.75 15.88 14.47 14.91 15.4 14.21L13.96 12.29M20 18V15H18V18H15V20H18V23H20V20H23V18H20Z" /></svg>';
 
 // meta-ns:meta
 var pluginName = "ImageFolder";
@@ -1036,7 +1036,7 @@ addStyle(`.if-button {
 }
 
 .if-button:hover {
-  background-color: var(--background-modifier-selected);
+  background-color: var(--interactive-background-selected);
 }
 
 .if-button svg {
@@ -1045,11 +1045,11 @@ addStyle(`.if-button {
 }
 
 .if-button path {
-  fill: var(--interactive-normal);
+  fill: var(--interactive-text-default);
 }
 
 .if-button:hover path {
-  fill: var(--interactive-hover);
+  fill: var(--interactive-text-hover);
 }
 
 .if-view {
