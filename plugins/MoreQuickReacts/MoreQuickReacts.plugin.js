d2456c725247ad32b8df410834913f8f693bb08e
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-08-18 11:27:20 -0400

    Bump version

--- plugins/MoreQuickReacts/MoreQuickReacts.plugin.js
+++ plugins/MoreQuickReacts/MoreQuickReacts.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name MoreQuickReacts
  * @description Increases the number of quick reactions available when hovering over a message, and pin ones of your choosing
- * @version 1.2.2
+ * @version 1.2.3
  * @author TheLazySquid
  * @authorId 619261917352951815
  * @website https://github.com/TheLazySquid/BetterDiscordPlugins
